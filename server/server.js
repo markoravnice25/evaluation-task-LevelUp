f435b3c264b288597600fb0c4ba3d18d0e3c2946
markoravnice25 <markoravnice25@gmail.com>
2022-08-23 10:26:37 +0200

    back and front end connected

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get("/api", (req, res) => {
-  res.json({ "users": ["userOne", "userTwo", "userThree", "anotherUser", "testingAgain"] })
+  res.json({ "users": ["userOne", "userTwo", "userThree", "anotherUser", "testingAgain", "testingOpet"] })
 })
 
 app.listen(5003, () => { console.log("server started on port 5003") })
