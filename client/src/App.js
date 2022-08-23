260548de793511e23cb9200dfd4f92f773ca98d9
markoravnice25 <markoravnice25@gmail.com>
2022-08-23 10:14:55 +0200

    setup front end

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+
+function App() {
+
+  const [testData, setTestData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setTestData(data)
+      }
+    )
+  }, [])
+
+  return (
+    <div>
+
+      {(typeof testData.users === 'undefined') ? (
+        <p>Loading...</p>
+      ) : (
+        testData.users.map((user, i) => (
+          <p key={i}>{user}</p>
+        ))
+      )}
+
+    </div>
+  )
+}
+
+export default App
