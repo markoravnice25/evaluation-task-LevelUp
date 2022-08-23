8ed0d83e7a31cb43d1585636858c82b8761b4583
markoravnice25 <markoravnice25@gmail.com>
2022-08-23 13:05:32 +0200

    front and back end functionality complete for parts 1, 2, 3.

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,71 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
+
+import Button from 'react-bootstrap/esm/Button'
+import Row from 'react-bootstrap/Row'
+import Form from 'react-bootstrap/Form'
+import Col from 'react-bootstrap/esm/Col'
+import axios from 'axios'
 
 function App() {
 
-  const [testData, setTestData] = useState([{}])
+  const [formData, setFormData] = useState({
+    pan: '',
+    expiryDate: '',
+    cvv: '',
+  })
 
-  useEffect(() => {
-    fetch("/validate-credit-card").then(
-      response => response.json()
-    ).then(
-      data => {
-        setTestData(data)
+  const [errors, setErrors] = useState(false)
+  const [status, setStatus] = useState('')
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+    setErrors(false)
+    setStatus('')
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await axios.post('/validate-credit-card', formData)
+      console.log(response)
+      if(response.data.isValid) {
+        setStatus('✅')
+      } else {
+        setStatus('❌')
       }
-    )
-  }, [])
+    } catch (error) {
+      console.log(error)
+      setErrors(error)
+    }
+  }
 
   return (
     <div>
-
-      {(typeof testData.users === 'undefined') ? (
-        <p>Loading...</p>
-      ) : (
-        testData.users.map((user, i) => (
-          <p key={i}>{user}</p>
-        ))
-      )}
-
+      <Form className='auth-login' onSubmit={handleSubmit}>
+        <Row className="mb-3 form-label">
+          <Row>
+            <h3 className='login-heading'>Credit card check</h3>
+          </Row>
+          <Form.Group as={Col}>
+            <Form.Label>CC number</Form.Label>
+            <Form.Control type="text" name='pan' value={formData.pan} onChange={handleChange} />
+          </Form.Group>
+          <Form.Group as={Col}>
+            <Form.Label>Expiry Date</Form.Label>
+            <Form.Control type="text" name='expiryDate' value={formData.expiryDate} onChange={handleChange} />
+          </Form.Group>
+          <Form.Group as={Col}>
+            <Form.Label>CVV</Form.Label>
+            <Form.Control type="text" name='cvv' value={formData.cvv} onChange={handleChange} />
+            {errors && <p className='text-danger'>{errors}ERROR</p>}
+          </Form.Group>
+          <Form.Group>
+            <Button className='button-login' type="submit">
+              Submit{status}
+            </Button>
+          </Form.Group>
+        </Row>
+      </Form>
     </div>
   )
 }
