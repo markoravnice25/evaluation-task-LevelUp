67596a260ec48eccac4e63e4a13ac8ba726f8626
markoravnice25 <markoravnice25@gmail.com>
2022-08-26 11:49:43 +0200

    Updated indentation and comments on back end

--- server/server.js
+++ server/server.js
@@ -5,9 +5,11 @@ app.use(express.json()) // accessing the JSON body on req.body
 // POST method used
 app.post("/validate-credit-card", (req, res) => {
   
-  // global variables
+  // destructured body from request object
   const { body } = req
+  // creating empty resJSON object
   let resJSON = {}
+  // extract payload properties from CC POST
   const expiryDate = body.expiryDate
   const cvv = body.cvv
   const pan = body.pan
@@ -27,7 +29,7 @@ app.post("/validate-credit-card", (req, res) => {
   res.json(resJSON)
 })
 
-// TODO 1. The expiry date of the credit card (year and month) must be AFTER present time
+// * 1. The expiry date of the credit card (year and month) must be AFTER present time
 const validateExpiryDate = (expiryDate, resJSON) => {
   if(expiryDate.length === 4) {
     const month = expiryDate.slice(0, 2)
@@ -48,7 +50,7 @@ const validateExpiryDate = (expiryDate, resJSON) => {
   return resJSON
 }
 
-// TODO 2. The CVV (security code) of the credit card must be exactly 3 digits long
+// * 2. The CVV (security code) of the credit card must be exactly 3 digits long
 // Unless it’s an American Express card, in which case the CVV must be exactly 4 digits long
 // American Express are cards whose PAN (card numbers) starts with either “34” or “37”
 // function checks pan length and that it is a digit
@@ -65,24 +67,24 @@ const validateCVV = (pan, cvv, resJSON) => {
   return resJSON
 }
 
-// TODO 3. The PAN (card number) is between 16 and 19 digits long
+// * 3. The PAN (card number) is between 16 and 19 digits long
 // function checks pan length and that it is a digit
 const validatePan = (pan, resJSON) => {
   if(pan.length < 16 || pan.length > 19 || isNaN(pan)) {
     resJSON.panError = 'Credit card number must be between 16 and 19 digits long'
   }
 
-  // TODO 4. Last digit of the PAN (card number) is checked using Luhn’s algorithm
+  // * 4. Last digit of the PAN (card number) is checked using Luhn’s algorithm
   let arr = (pan + '')
-  .split('')
-  .reverse()
-  .map(x => parseInt(x))
-let lastDigit = arr.splice(0, 1)[0]
-let sum = arr.reduce((acc, val, i) => (i % 2 !== 0 ? acc + val : acc + ((val * 2) % 9) || 9), 0)
-sum += lastDigit
-if(sum % 10 !== 0) {
-  resJSON.luhnError = 'Invalid Credit Card number'
-}
+    .split('')
+    .reverse()
+    .map(x => parseInt(x))
+  let lastDigit = arr.splice(0, 1)[0]
+  let sum = arr.reduce((acc, val, i) => (i % 2 !== 0 ? acc + val : acc + ((val * 2) % 9) || 9), 0)
+  sum += lastDigit
+  if(sum % 10 !== 0) {
+    resJSON.luhnError = 'Invalid Credit Card number'
+  }
 
   return resJSON
 }
